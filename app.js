979964d12b8efb455888810e79fb77d63fcd88ca
Lincoln Dennis <j.lincoln.dennis@gmail.com>
2016-04-03 17:12:56 -0600

    Login might be working with Tumblr

--- app.js
+++ app.js
@@ -3,12 +3,40 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
+var cookieSession = require('cookie-session');
 var bodyParser = require('body-parser');
+require('dotenv').load()
 
+var passport = require('passport');
+var TumblrStrategy = require('passport-tumblr').Strategy;
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+// Use the TumblrStrategy within Passport.
+//   Strategies in passport require a `verify` function, which accept
+//   credentials (in this case, a token, tokenSecret, and Tumblr profile), and
+//   invoke a callback with a user object.
+passport.use(new TumblrStrategy({
+    consumerKey: process.env.TUMBLR_CONSUMER_KEY,
+    consumerSecret: process.env.TUMBLR_SECRET_KEY,
+    callbackURL: "http://localhost:3000/auth/tumblr/callback"
+  },
+  function(token, tokenSecret, profile, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+
+      // To keep the example simple, the user's Tumblr profile is returned to
+      // represent the logged-in user.  In a typical application, you would want
+      // to associate the Tumblr account with a user record in your database,
+      // and return that user instead.
+      return done(null, profile);
+    });
+  }
+));
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,9 +48,42 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cookieSession({
+  name: 'session',
+  keys: [
+    process.env.SESSION_KEY1,
+    process.env.SESSION_KEY2,
+    process.env.SESSION_KEY3
+  ]
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user)
+});
+
 app.use('/', routes);
+
+app.get('/auth/tumblr',
+  passport.authenticate('tumblr'),
+  function(req, res){
+    // The request will be redirected to Tumblr for authentication, so this
+    // function will not be called.
+  });
+
+app.get('/auth/tumblr/callback',
+  passport.authenticate('tumblr', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
 app.use('/users', users);
 
 // catch 404 and forward to error handler
