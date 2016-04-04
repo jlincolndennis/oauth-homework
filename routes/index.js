720f80d44d738014c93d0eb451084adfa176689f
Lincoln Dennis <j.lincoln.dennis@gmail.com>
2016-04-03 18:39:21 -0600

    Logout works locally

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', username: req.session.passport.user});
+  res.render('index', { title: 'Tumblr and OAuth!'});
 });
 
 router.get('/login', function(req, res, next){
   res.render('login')
+})
+
+router.get('/logout', function(req, res, next){
+  req.session = null;
+  res.redirect('/')
 })
 
 module.exports = router;
